4c2439e90f22a22e88e2b0c69c386206d620e986
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-10-09 16:17:57 -0400

    Install 'classNames' and create shell for Issues page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { MdBugReport } from "react-icons/md";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const navLinks = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" }
@@ -15,7 +20,14 @@ const NavBar = () => {
       <ul className='flex gap-6'>
         {navLinks.map((link) => (
           <li key={link.href}>
-            <Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true
+              })}
+              href={link.href}
+            >
               {link.label}
             </Link>
           </li>
